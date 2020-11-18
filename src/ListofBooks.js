78def4c73992d70879002faaa07c9d3369a7ea25
badiou <ourobadiou@gmail.com>
2020-11-18 16:59:28 +0000

    Create ListOfBooks and SearchPage

---
+++ src/ListofBooks.js
@@ -0,0 +1,56 @@
+// @flow
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+import BookShelf from "./BookShelf";
+import * as BooksAPI from "./BooksAPI";
+
+
+class ListBooks extends React.Component {
+  state = {};
+
+  handleChangeBookShelf = (bookId: int, e: any) => {
+    const etagere = this.props.booksOnShelf;
+    const book = etagere.filter(t => t.id === bookId)[0];
+    book.shelf = e.target.value;
+    BooksAPI.update(book, e.target.value).then(response => {
+      this.setState({
+        books: etagere
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <BookShelf
+            key="currently"
+            books={this.props.booksOnShelf.filter(book => book.shelf === "currentlyReading")}
+            onChangeShelf={this.handleChangeBookShelf}
+            shelftitle="Currently Reading"
+          />
+          <BookShelf
+            key="wantToRead"
+            books={this.props.booksOnShelf.filter(book => book.shelf === "wantToRead")}
+            onChangeShelf={this.handleChangeBookShelf}
+            shelftitle="Want to Read"
+          />
+          <BookShelf
+            key="read"
+            books={this.props.booksOnShelf.filter(book => book.shelf === "read")}
+            onChangeShelf={this.handleChangeBookShelf}
+            shelftitle="Read"
+          />
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+    );
+  }
+}
+export default ListBooks;
